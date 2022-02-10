7b5878089cb45a4b6b2709926b911a79f94c7fc6
sanuja gayantha <sanuja@pop-os.localdomain>
2022-02-10 16:17:27 +0530

    commit

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
         e.preventDefault();
         let searchText = $('#searchText').val();
-        
+        console.log(searchText);
     })
 })
